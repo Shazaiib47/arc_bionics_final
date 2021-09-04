40e705dc73b22bbb0eff45f4a2c925856e81c78a
Shazaiib47 <shazaib47@hotmail.com>
2021-09-04 00:26:13 +0000

    added another image to be used and finished the add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -17,8 +17,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile has been updated successfully.')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update field. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
