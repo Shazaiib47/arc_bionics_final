4874912569c2c0dc6b0890b040f2ca3ae2ac4f37
Shazaiib47 <shazaib47@hotmail.com>
2021-09-04 05:06:17 +0000

    removed the secret key from settings and set the debug

--- arc_bionics_final/settings.py
+++ arc_bionics_final/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ey@ml0al3r9g_p8s#grj5d*nt5%_2h9n^xog5#j#&3vrj0g9^)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['arc-bionics-final.herokuapp.com', 'localhost']
 
