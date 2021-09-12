e5ec6de95102945b9c519803ba68c9c0342f1396
Shazaiib47 <shazaib47@hotmail.com>
2021-09-12 03:28:55 +0000

    renamed home to blog to match url and also added the blog url in main_nav as i forgot to commit that too

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from .import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.all_posts, name='home'),
+    path('', views.all_posts, name='blog'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
