11fe6563ecc9eab317967387f1b23c4433617d28
Shazaiib47 <shazaib47@hotmail.com>
2021-09-14 00:00:16 +0000

    implemented the models for contact model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """ models and fields for contact model"""
+    subject = models.CharField(max_length=130, null=False, blank=False)
+    email = models.EmailField(max_length=150, null=False, blank=False)
+    message = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.subject
